2327064a9315e2620560f9d9c8e943d08a45e850
Anton <savchenko.a@husky.neu.edu>
2012-03-09 16:02:45 -0500

    bla

--- resources/js/content.js
+++ resources/js/content.js
@@ -12,10 +12,37 @@ function choose(){
 
 function onSuccess(imageData) {
     console.log('image successfully taken');
+    currentImage = imageData;
     var image = document.getElementById('image');
-    image.src = "data:image/jpeg;base64," + imageData;
+    image.src = "data:image/jpeg;base64," + currentImage;
 }
 
 function onFail(message) {
     alert('Failed because: ' + message);
+}
+
+function submit(){
+
+    console.log('submit called');
+
+    $.ajax({
+        url: 'http://api.imgur.com/2/upload.json',
+        type: 'POST',
+        data: {
+            key: API_KEY,
+            type: 'base64',
+            name: 'test',
+            title: 'imgur upload test',
+            caption: 'uploaded from android',
+            image: currentImage
+        },
+        success: function(data){
+            console.log('ajax success');
+            console.log(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log('ajax fail');
+            alert('Imgur submission failed because: ' + textStatus);
+        }
+    })
 }
