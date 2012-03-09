94b19d43f6ed4e0b41684eee347f2063703966c0
Anton <savchenko.a@husky.neu.edu>
2012-03-09 14:47:16 -0500

    bla

--- resources/js/content.js
+++ resources/js/content.js
@@ -1,12 +1,21 @@
 var API_KEY = 'ecb3c9429017e1310b52e91afeb50175';
 
+console.log('running js');
+
+document.addEventListener("deviceready", function(){
+    console.log('device ready');
+}, false);
+
+console.log('setting up snap');
 function snap(){
     console.log('snap called');
-    navigator.camera.getPicture(onSuccess, onFail, { quality: 50 });
+    navigator.camera.getPicture(onSuccess, onFail, {});
 }
 
 function onSuccess(imageData) {
     console.log('image successfully taken');
+    var image = document.getElementById('image');
+    image.src = "data:image/jpeg;base64," + imageData;
 }
 
 function onFail(message) {
