16a8dca948965fa2f5ef42abc3218099bc11ffd7
Anton <savchenko.a@husky.neu.edu>
2012-03-09 16:25:33 -0500

    bla

--- resources/js/content.js
+++ resources/js/content.js
@@ -3,25 +3,37 @@ var currentImage;
 
 
 function snap(){
-    navigator.camera.getPicture(onSuccess, onFail, {destinationType : Camera.DestinationType.DATA_URL});
+    console.log('snap called');
+    navigator.camera.getPicture(submit, onFail, {
+        sourceType : Camera.PictureSourceType.CAMERA,
+        destinationType : Camera.DestinationType.DATA_URL,
+        targetWidth: 100,
+        targetHeight: 100
+    });
 }
 
 function choose(){
     console.log('choose called');
+    navigator.camera.getPicture(submit, onFail, {
+        sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+        destinationType : Camera.DestinationType.DATA_URL,
+        targetWidth: 100,
+        targetHeight: 100
+    });
 }
 
 function onSuccess(imageData) {
     console.log('image successfully taken');
-    currentImage = imageData;
-    var image = document.getElementById('image');
-    image.src = "data:image/jpeg;base64," + currentImage;
+//    currentImage = imageData;
+    /*var image = document.getElementById('image');
+    image.src = "data:image/jpeg;base64," + imageData;*/
 }
 
 function onFail(message) {
     alert('Failed because: ' + message);
 }
 
-function submit(){
+function submit(imgData){
 
     console.log('submit called');
 
@@ -34,7 +46,7 @@ function submit(){
             name: 'test',
             title: 'imgur upload test',
             caption: 'uploaded from android',
-            image: currentImage
+            image: imgData
         },
         success: function(data){
             console.log('ajax success');
