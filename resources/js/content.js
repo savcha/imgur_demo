0ba2999837eb74b7b33d8ed1785fdfc6660f70c3
Anton <savchenko.a@husky.neu.edu>
2012-03-09 15:42:21 -0500

    bla

--- resources/js/content.js
+++ resources/js/content.js
@@ -1,24 +1,9 @@
 var API_KEY = 'ecb3c9429017e1310b52e91afeb50175';
+var currentImage;
 
-console.log('running js');
 
-document.addEventListener("deviceready", function(){
-    console.log('device ready');
-}, false);
-
-console.log('setting up snap');
 function snap(){
-    console.log('snap called');
-
-    if(navigator){
-        console.log('navigator defined');
-        if(navigator.camera){
-            console.log('camera defined');
-            navigator.camera.getPicture(onSuccess, onFail, {});
-        }
-        else console.log('camera not defined');
-    }
-    else console.log('navigator not defined');
+    navigator.camera.getPicture(onSuccess, onFail, {destinationType : Camera.DestinationType.DATA_URL});
 }
 
 function choose(){
@@ -27,9 +12,8 @@ function choose(){
 
 function onSuccess(imageData) {
     console.log('image successfully taken');
-    var image = document.createElement('img');//document.getElementById('image');
+    var image = document.getElementById('image');
     image.src = "data:image/jpeg;base64," + imageData;
-    document.getElementsByTagName('body')[0].appendChild(image);
 }
 
 function onFail(message) {
